31f0629b93a40093e7e41d1beca0b031a35be046
Marcus W <marcuswsywong@gmail.com>
2024-03-16 21:26:24 +0000

    Refactor and UI update
    Update to CSS to style and change color scheme
    Refactor existing constructor functions into classes

--- script.js
+++ script.js
@@ -9,66 +9,77 @@ const inputAuthor = document.querySelector("#author")
 const inputPages =  document.querySelector("#pages")
 const inputRead =  document.querySelector("#read-or-unread")
 
-// create a card in html
+// create library class 
+class Library {
+    constructor() {
+        this.books = []
+    }
 
-const createCard = (book) => {
-    const card = document.createElement("div")
-    card.classList.add("book-card")
-    const title = document.createElement("p")
-    title.textContent = `Title: ${book.title}`;
-    const author = document.createElement("p")
-    author.textContent = `Author: ${book.author}`;
-    const pages = document.createElement("p")
-    pages.textContent = `Pages: ${book.pages}`;
-    const read = document.createElement("p")
-    read.textContent = `${book.read}`;
+    addBook(book) {
+        this.books.push(book)
+    }
 
-    const cardBtnContainers = document.createElement("div")
-    cardBtnContainers.classList.add("card-btns")
-    const removeBtn = document.createElement("button")
-    removeBtn.textContent = "Remove"
-    removeBtn.addEventListener("click", () => {card.classList.add("hidden")})
-    const readToggleBtn = document.createElement("button")
-    readToggleBtn.textContent = `${book.read}`
-    readToggleBtn.addEventListener("click", () => {
-        if (book.read === "Read") {
-            book.read = "Unread"
-        } else {
-            book.read = "Read"
-        }
-        readToggleBtn.textContent = book.read
-    });
+    displayLibrary(library) {
+        library.map((book) => this.createCard(book))
+    }
+
+    createCard(book) {
+        const card = document.createElement("div")
+        card.classList.add("book-card")
+        const title = document.createElement("p")
+        title.textContent = `${book.title}`;
+        title.classList.add("book-main-info")
+        const author = document.createElement("p")
+        author.textContent = `${book.author}`;
+        author.classList.add("book-main-info")
+        const pages = document.createElement("p")
+        pages.textContent = `Pages: ${book.pages}`;
+        pages.classList.add("book-side-info")
+        const read = document.createElement("p")
+        read.textContent = `${book.read}`;
+        read.classList.add("book-side-info")
     
-    card.appendChild(title)
-    card.appendChild(author)
-    card.appendChild(pages)
-    card.appendChild(read)
-    card.appendChild(cardBtnContainers)
-    cardBtnContainers.appendChild(removeBtn)
-    cardBtnContainers.appendChild(readToggleBtn)
+        const cardBtnContainers = document.createElement("div")
+        cardBtnContainers.classList.add("card-btns")
+        const removeBtn = document.createElement("button")
+        removeBtn.textContent = "Remove"
+        removeBtn.addEventListener("click", () => {card.classList.add("hidden")})
+        const readToggleBtn = document.createElement("button")
+        readToggleBtn.textContent = `${book.read}`
+        readToggleBtn.addEventListener("click", () => {
+            if (book.read === "Read") {
+                book.read = "Unread"
+            } else {
+                book.read = "Read"
+            }
+            readToggleBtn.textContent = book.read
+        });
+        
+        card.appendChild(title)
+        card.appendChild(author)
+        card.appendChild(pages)
+        card.appendChild(read)
+        card.appendChild(cardBtnContainers)
+        cardBtnContainers.appendChild(removeBtn)
+        cardBtnContainers.appendChild(readToggleBtn)
+    
+        cardContainer.appendChild(card)
 
-    cardContainer.appendChild(card)
+        return card;
+    }
 }
 
+// class for a book
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = read
+    }
+}
 
-const myLibrary = [];
-const addBookToLibrary = (book) => {
-  myLibrary.push(book)
-}
-const displayLibrary = (library) => {
-  library.map((book) => createCard(book))
-}
-
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.info = function() {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    },
-    this.createBookCard = createCard
-}
+const library = new Library
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Unread")
 const theHobbit2 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 324, "Read")
@@ -85,9 +96,9 @@ addBookBtn.addEventListener("click", addBookForm)
 
 const submitBook = () => {
     let newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.value)
-    myLibrary.push(newBook)
-    createCard(newBook)
-    console.log(myLibrary)
+    library.books.push(newBook)
+    library.createCard(newBook)
+    console.log(library.books)
 }
 
 modalConfirmBtn.addEventListener("click", (e) => {
@@ -100,6 +111,6 @@ modalConfirmBtn.addEventListener("click", (e) => {
     }
 })
 
-addBookToLibrary(theHobbit)
-addBookToLibrary(theHobbit2)
-displayLibrary(myLibrary)
+library.addBook(theHobbit)
+library.addBook(theHobbit2)
+library.displayLibrary(library.books)
